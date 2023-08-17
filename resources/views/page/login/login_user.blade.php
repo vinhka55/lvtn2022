f7af7ae32949ed0beec3da5451fac8eb6340d709
LE HUU VINH <vinhphac2@gmail.com>
2023-08-17 18:11:51 +0700

    update feature timezone in notification

--- resources/views/page/login/login_user.blade.php
+++ resources/views/page/login/login_user.blade.php
@@ -42,20 +42,20 @@
               <h2 class="p-2 m-0 bg-success text-white">Đăng kí mới</h2>
               
               <form action="{{route('register_customer')}}" method="post">
-                              @csrf
-                <input class="form-control mt-1" type="text" name='name' placeholder="Name"/>
-                <input class="form-control mt-1" type="email" name='email' placeholder="Email Address"/>
-                <input class="form-control mt-1" type="password" name='password' placeholder="Password"/>
-                              <input class="form-control mt-1" type="password" name='repassword' placeholder="Retype Password"/>
-                              <input class="form-control mt-1" type="text" name='phone' placeholder="Phone"/>
-                              <div class="g-recaptcha" data-sitekey="{{env('CAPTCHA_KEY')}}"></div>
-                              <br/>
-                              @if($errors->has('g-recaptcha-response'))
-                              <span class="invalid-feedback mt-1" style="display:block;">
-                                  <strong>{{$errors->first('g-recaptcha-response')}}</strong>
-                              </span>
-                              @endif
-                <button type="submit" class="btn btn-primary mt-1">Signup</button>
+                  @csrf
+                  <input class="form-control mt-1" type="text" name='name' placeholder="Name"/>
+                  <input class="form-control mt-1" type="email" name='email' placeholder="Email Address"/>
+                  <input class="form-control mt-1" type="password" name='password' placeholder="Password"/>
+                  <input class="form-control mt-1" type="password" name='repassword' placeholder="Retype Password"/>
+                  <input class="form-control mt-1" type="text" name='phone' placeholder="Phone"/>
+                  <div class="g-recaptcha" data-sitekey="{{env('CAPTCHA_KEY')}}"></div>
+                  <br/>
+                  @if($errors->has('g-recaptcha-response'))
+                  <span class="invalid-feedback mt-1" style="display:block;">
+                      <strong>{{$errors->first('g-recaptcha-response')}}</strong>
+                  </span>
+                  @endif
+                  <button type="submit" class="btn btn-primary mt-1">Signup</button>
               </form>
             </div><!--/sign up form-->
           </div>
